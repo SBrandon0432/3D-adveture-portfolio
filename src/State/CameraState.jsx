cfaf36dc602f3540a30ecfc1c8e729cd47259e4c
sbrandon0432 <sbrandon0432@yahoo.com>
2022-07-12 20:31:04 -0700

    update

--- src/State/CameraState.jsx
+++ src/State/CameraState.jsx
@@ -2,8 +2,8 @@ import * as THREE from 'three'
 
 const CameraState = {
   cameraPos: new THREE.Vector3(-7,-1.2,4),
-  cameraPos1: new THREE.Vector3(-45,0,7),
-  target1: new THREE.Vector3(-40,0,0),
+  cameraPos1: new THREE.Vector3(-51,1,7),
+  target1: new THREE.Vector3(-40,0,-3),
 }
 
 export default CameraState;
