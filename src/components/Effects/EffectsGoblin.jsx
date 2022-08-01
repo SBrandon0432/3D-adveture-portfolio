ee60aa7b9ed3de79473dc638dc5657278d71ddc4
sbrandon0432 <sbrandon0432@yahoo.com>
2022-08-01 16:12:31 -0700

    dependicy issues:

--- src/components/Effects/EffectsGoblin.jsx
+++ src/components/Effects/EffectsGoblin.jsx
@@ -1,7 +1,7 @@
 import React,{Suspense, useState} from 'react';
 import * as THREE from 'three'
 import {useFrame} from '@react-three/fiber';
-import { EffectComposer, DepthOfField, Bloom, Vignette, GodRays } from '@react-three/postprocessing'
+// import { EffectComposer, DepthOfField, Bloom, Vignette, GodRays } from '@react-three/postprocessing'
 import { Cloud, Effects } from '@react-three/drei';
 
 
@@ -9,11 +9,12 @@ const EffectsGoblin = (props) => {
 
 
   return (
-    <group>
-    <EffectComposer>
-       <DepthOfField focusDistance={0} focalLength={0.06} bokehScale={2} height={480} />
-    </EffectComposer>
-    </group>
+    // <group>
+    // <EffectComposer>
+    //    <DepthOfField focusDistance={0} focalLength={0.06} bokehScale={2} height={480} />
+    // </EffectComposer>
+    // </group>
+    null
   )
 }
 
