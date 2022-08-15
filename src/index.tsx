7035ae59a0680c00b6333b6a91f0c84a0337f589
sbrandon0432 <sbrandon0432@yahoo.com>
2022-08-15 16:32:43 -0700

    code clean up

--- src/index.tsx
+++ src/index.tsx
@@ -11,29 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-// "@react-three/cannon": "^6.3.0",
-// "@react-three/drei": "^9.13.2",
-// "@react-three/fiber": "^8.0.26",
-// "@react-three/postprocessing": "^2.5.5",
-// "@testing-library/jest-dom": "^5.16.4",
-// "@testing-library/react": "^13.3.0",
-// "@testing-library/user-event": "^13.5.0",
-// "@types/dat.gui": "^0.7.7",
-// "@types/jest": "^27.5.2",
-// "@types/node": "^16.11.41",
-// "@types/react": "^18.0.14",
-// "@types/react-dom": "^18.0.5",
-// "@types/three": "^0.141.0",
-// "dat.gui": "^0.7.9",
-// "react": "^18.2.0",
-// "react-dom": "^18.2.0",
-// "react-scripts": "^2.1.3",
-// "react-three-fiber": "^6.0.13",
-// "sass": "^1.52.3",
-// "serve": "^13.0.2",
-// "three": "^0.141.0",
-// "typescript": "^4.7.3",
-// "web-vitals": "^2.1.4",
-// "zustand": "^3.7.2"
